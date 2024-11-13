e3c716051bd47700e3fecc982f34ab728492b618
antowester <awestermeier@miuandes.cl>
2024-11-13 14:38:14 -0300

    metricas para usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,7 @@ Rails.application.routes.draw do
 
   resources :superusuario, only: [:index]
   resources :modelos 
-  resources :metricas 
+  resources :metricas, only: [:index]
   resources :casilleros, only: [:index, :show] # Index y show de casilleros fuera de contexto de un controlador
 
 
